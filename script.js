b1b61d96b97454644d72d881b00624a68426efe5
Architect <greatraksin@icloud.com>
2020-11-04 14:17:52 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function turnLightOn() {
+    document.getElementById('myImage').src='images/pic_bulbon.gif'
+}
+
+function turnLightOff() {
+    document.getElementById('myImage').src='images/pic_bulboff.gif'
+}
